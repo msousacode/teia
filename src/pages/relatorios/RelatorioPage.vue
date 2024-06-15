2d9a62b5d353ea450255f28cab6a11ace51c9c73
Michael Sousa <msousacode@gmail.com>
2024-06-15 09:11:36 -0300

    dev

--- src/pages/relatorios/RelatorioPage.vue
+++ src/pages/relatorios/RelatorioPage.vue
@@ -4,45 +4,58 @@
             <div class="col-12 text-center">
                 <p class="text-h6 text-teal-7">Gerar Relatório</p>
             </div>
-
-            {{ form }}
             <q-form class="col-md-7 col-xs-12 col-sm-12">
                 <q-select outlined v-model="form.aprendiz" :options="aprendizes" label="Selecione o Aprendiz"
                     @input="pesquisar" />
             </q-form>
         </div>
 
-        <div class="q-pa-md">
-            <div class="text-body1 q-mb-sm">Selecione o dia para coleta:</div>
-            <q-list bordered separator v-for="(
-              item, index
-            ) in treinamentos" :key="index">
-                <q-item clickable v-ripple>
-                    <q-item-section>
-                        <q-item-label class="text-subtitle1 q-mb-sm">{{ item.treinamento }}</q-item-label>
-                        <q-item-label class="text-subtitle1 q-mt-sm">{{ item.protocolo }}</q-item-label>
-
-                        <div v-if="item.configuracoes">
-                            <q-item-label class="text-subtitle1 q-mt-sm">Repete: {{ item.configuracoes.repetir
-                                }}</q-item-label>
-                            <q-item-label>
-                                <!-- <q-chip v-if="item.configuracoes.dias_semana" v-for="(
-                                  dia, index
-                                ) in item.configuracoes.dias_semana" :key="index" label="dia" /> -->
-                            </q-item-label>
-                        </div>
-                    </q-item-section>
-                </q-item>
-            </q-list>
+        <div class="q-mt-sm row items-start q-gutter-md">
+            <q-card class="my-card bg-green text-white">
+                <q-card-section>
+                    <div class="text-subtitle1">FEZ SEM AJUDA
+                        <br /> INDEPENDÊNCIA
+                    </div>
+                    <div class="text-h6">70%</div>
+                </q-card-section>
+            </q-card>
+            <q-card class="my-card bg-red-5 text-white">
+                <q-card-section>
+                    <div class="text-subtitle1">FEZ COM AJUDA <br /> OU NÃO FEZ</div>
+                    <div class="text-h6">30%</div>
+                </q-card-section>
+            </q-card>
         </div>
 
+        <div class="text-body1 q-mb-sm q-mt-md text-teal-7 text-uppercase">Treinamentos em andamento:</div>
+        <q-list bordered separator v-for="(
+              item, index
+            ) in treinamentos" :key="index">
+            <q-item clickable v-ripple>
+                <q-item-section>
+                    <q-item-label class="text-subtitle1 q-mb-sm">{{ item.treinamento }}</q-item-label>
+                    <q-item-label class="text-subtitle1 q-mb-sm" caption>Início: 15/06/2024</q-item-label>
+                    <q-item-label class="text-subtitle1 q-mb-sm" caption>Final: 15/06/2024</q-item-label>
+                    <q-item-label>
+                        Progresso:
+                    </q-item-label>
+                    <q-linear-progress size="25px" :value="progress1" color="green-5">
+                        <div class="absolute-full flex flex-center">
+                            <q-badge color="white" text-color="accent" :label="progressLabel1" />
+                        </div>
+                    </q-linear-progress>
+                </q-item-section>
+            </q-item>
+        </q-list>
+
         <div class="row justify-center">
-            <q-btn label="Gerar Relatório" color="info" class="col-md-7 col-xs-12 col-sm-12" @click="pesquisar" />
+            <q-btn label="Gerar Relatório" color="info" class="col-md-7 col-xs-12 col-sm-12 q-mt-xl"
+                @click="pesquisar" />
         </div>
     </q-page>
 </template>
 <script setup lang="ts">
-import { onMounted, ref, toRaw } from 'vue';
+import { computed, onMounted, ref, toRaw } from 'vue';
 import { db } from 'src/db'
 
 const form = ref({
@@ -55,9 +68,12 @@ const treinamentos = ref<any[]>([]);
 
 const atendimentos = ref<any[]>([]);
 
+const progress1 = ref(0.3)
+
+let progressLabel1 = computed(() => (progress1.value * 100).toFixed(2) + '%');
+
 function pesquisar() {
     const raw = toRaw(form.value);
-    debugger
 
     db.atendimentos.where({ aprendiz_uuid_fk: raw.aprendiz.value }).toArray().then(res => {
         atendimentos.value = toRaw(res);
