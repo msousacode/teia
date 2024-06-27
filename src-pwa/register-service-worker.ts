3e0382b0c5be366deadc2218e8874c1dfc9f914c
Michael Sousa <msousacode@gmail.com>
2024-06-27 14:55:26 -0300

    dev

--- src-pwa/register-service-worker.ts
+++ src-pwa/register-service-worker.ts
@@ -1,8 +1,14 @@
 import { register } from 'register-service-worker';
+import { Notify } from 'quasar';
 
 // The ready(), registered(), cached(), updatefound() and updated()
 // events passes a ServiceWorkerRegistration instance in their arguments.
 // ServiceWorkerRegistration: https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration
+
+window.addEventListener('online', () => {
+  // Requisitar atualização do service worker quando voltar a ficar online
+  console.log('Online');
+});
 
 register(process.env.SERVICE_WORKER_FILE, {
   // The registrationOptions object will be passed as the second argument
@@ -11,31 +17,39 @@ register(process.env.SERVICE_WORKER_FILE, {
 
   // registrationOptions: { scope: './' },
 
-  ready (/* registration */) {
+  ready(/* registration */) {
     // console.log('Service worker is active.')
   },
 
-  registered (/* registration */) {
+  registered(/* registration */) {
     // console.log('Service worker has been registered.')
   },
 
-  cached (/* registration */) {
+  cached(/* registration */) {
     // console.log('Content has been cached for offline use.')
   },
 
-  updatefound (/* registration */) {
+  updatefound(/* registration */) {
     // console.log('New content is downloading.')
   },
 
-  updated (/* registration */) {
-    // console.log('New content is available; please refresh.')
+  updated(/* registration */) {
+    Notify.create({
+      message: 'Nova atualização disponível',
+      icon: 'mdi-cloud-download-outline',
+      closeBtn: 'Atualizar',
+      timeout: 10000,
+      onDismiss() {
+        location.reload(true);
+      },
+    });
   },
 
-  offline () {
+  offline() {
     // console.log('No internet connection found. App is running in offline mode.')
   },
 
-  error (/* err */) {
+  error(/* err */) {
     // console.error('Error during service worker registration:', err)
   },
 });
