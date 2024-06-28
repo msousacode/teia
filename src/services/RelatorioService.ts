3dad0700283b808498ade8ce417ed83231eda51e
Michael Sousa <msousacode@gmail.com>
2024-06-28 14:14:42 -0300

    dev

--- src/services/RelatorioService.ts
+++ src/services/RelatorioService.ts
@@ -145,10 +145,7 @@ export class RelatorioService {
       `Relatório gerado em ${this.getDataAtual()}`
     );
 
-    const profissional = new Profissional(
-      'Catarina Renata da Silva',
-      'CRO 5406'
-    ); //TODO esses dados do profissional tem que vir do usuário logado, como essa parte não esta implementada, fica em todo, quando a tela de login estiver pronta eu terei essa informação.
+    const profissional = new Profissional('Nome Profisional', ''); //TODO esses dados do profissional tem que vir do usuário logado, como essa parte não esta implementada, fica em todo, quando a tela de login estiver pronta eu terei essa informação.
 
     const aprendiz = await this.getAprendiz(uuid);
 
