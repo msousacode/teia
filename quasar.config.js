48d74dc6045768d27c828f4f5f0effa7864ecfc5
Michael Sousa <msousacode@gmail.com>
2024-06-28 13:40:46 -0300

    dev

--- quasar.config.js
+++ quasar.config.js
@@ -203,7 +203,7 @@ module.exports = configure(function (ctx) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: 'unirsi',
+        appId: 'SysABA',
       },
     },
 
