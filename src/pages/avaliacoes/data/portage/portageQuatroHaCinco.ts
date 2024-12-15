5500a58b03350230647af5d17c24c11a025e7583
Michael Sousa <msousacode@gmail.com>
2024-12-14 21:46:41 -0300

    portage.

---
+++ src/pages/avaliacoes/data/portage/portageQuatroHaCinco.ts
@@ -0,0 +1,725 @@
+const portageQuatroHaCincoAno = {
+  nivel: 1,
+  avaliacoes: [
+    {
+      titulo: 'Socialização',
+      tipo: 1,
+      objetivos: [
+        {
+          id: 5001,
+          cod: '73',
+          descricao: 'Manifesta seus sentimentos: raiva, felicidade, amor.',
+          selected: null,
+        },
+        {
+          id: 5002,
+          cod: '74',
+          descricao:
+            'Brinca com 4 a 5 crianças em uma atividade de cooperação, sem supervisão constante.',
+          selected: null,
+        },
+        {
+          id: 5003,
+          cod: '75',
+          descricao: 'Explica aos outros regras do jogo ou atividade.',
+          selected: null,
+        },
+        {
+          id: 5004,
+          cod: '76',
+          descricao: 'Imita papéis de adultos.',
+          selected: null,
+        },
+        {
+          id: 5005,
+          cod: '77',
+          descricao: 'Colabora para a conversa durante as refeições.',
+          selected: null,
+        },
+        {
+          id: 5006,
+          cod: '78',
+          descricao: 'Segue regras de um jogo que envolva raciocínio verbal.',
+          selected: null,
+        },
+        {
+          id: 5007,
+          cod: '79',
+          descricao: 'Conforta coletas quando estão tristes.',
+          selected: null,
+        },
+        {
+          id: 5008,
+          cod: '80',
+          descricao: 'Escolhe os próprios amigos.',
+          selected: null,
+        },
+        {
+          id: 5009,
+          cod: '81',
+          descricao:
+            'Planeja e constrói usando ferramentas simples ( Ex: Plano inclinado, alavanca, roldana).',
+          selected: null,
+        },
+        {
+          id: 5010,
+          cod: '82',
+          descricao:
+            'Estabelece metas para si próprio e executa as atividades para atingi-las.',
+          selected: null,
+        },
+        {
+          id: 5011,
+          cod: '83',
+          descricao:
+            'Dramatiza trechos de estórias desempenhando papéis ou lidando com fantoches.',
+          selected: null,
+        },
+      ],
+    },
+
+    {
+      titulo: 'Cognição',
+      tipo: 2,
+      objetivos: [
+        {
+          id: 5012,
+          cod: '65',
+          descricao:
+            'Apanha um determinado número de objetos a pedidos (De 1 a 5).',
+          selected: null,
+        },
+        {
+          id: 5013,
+          cod: '66',
+          descricao: 'Nomeia 5 texturas diferentes.',
+          selected: null,
+        },
+        {
+          id: 5014,
+          cod: '67',
+          descricao: 'Copia um triângulo a ser requisitado.',
+          selected: null,
+        },
+        {
+          id: 5015,
+          cod: '68',
+          descricao: 'Recorda-se de 4 objetos vistos em uma gravura.',
+          selected: null,
+        },
+        {
+          id: 5016,
+          cod: '69',
+          descricao: 'Diz o momento do dia associado a cada atividade.',
+          selected: null,
+        },
+        {
+          id: 5017,
+          cod: '70',
+          descricao: 'Repete rimas familiares.',
+          selected: null,
+        },
+        {
+          id: 5018,
+          cod: '71',
+          descricao:
+            'Diz se um objeto é mais pesado ou mais leve (diferença de no máximo 0,5 Kg).',
+          selected: null,
+        },
+        {
+          id: 5019,
+          cod: '72',
+          descricao:
+            'Diz o que falta em quando objeto é retirado em um grupo de 3.',
+          selected: null,
+        },
+        {
+          id: 5020,
+          cod: '73',
+          descricao: 'Nomeia 8 cores.',
+          selected: null,
+        },
+        {
+          id: 5021,
+          cod: '74',
+          descricao: 'Identifica o valor de 3 moedas.',
+          selected: null,
+        },
+        {
+          id: 5022,
+          cod: '75',
+          descricao: 'Emparelhas símbolos (letras e números).',
+          selected: null,
+        },
+        {
+          id: 5023,
+          cod: '76',
+          descricao: 'Diz a cor de objetos nomeados.',
+          selected: null,
+        },
+        {
+          id: 5024,
+          cod: '77',
+          descricao:
+            'Relata 5 principais fatos de uma história contada 3 vezes.',
+          selected: null,
+        },
+        {
+          id: 5025,
+          cod: '78',
+          descricao: 'Desenha a figura humana (Cabeça, tronco e 4 membros).',
+          selected: null,
+        },
+        {
+          id: 5026,
+          cod: '79',
+          descricao: 'Canta 5 estrofes de uma canção.',
+          selected: null,
+        },
+        {
+          id: 5027,
+          cod: '80',
+          descricao: 'Constrói uma pirâmide de 10 blocos imitando o adulto.',
+          selected: null,
+        },
+        {
+          id: 5028,
+          cod: '81',
+          descricao: 'Nomeia objetos como “compridos” e “curtos”.',
+          selected: null,
+        },
+        {
+          id: 5029,
+          cod: '82',
+          descricao:
+            'Coloca objetos “atrás”, “ao lado” e “junto” a um outro objeto.',
+          selected: null,
+        },
+        {
+          id: 5030,
+          cod: '83',
+          descricao: 'Faz conjuntos iguais de 1 a 10 objetos.',
+          selected: null,
+        },
+        {
+          id: 5031,
+          cod: '84',
+          descricao:
+            'Nomeia ou aponta para a parte ausente de figura de um objeto.',
+          selected: null,
+        },
+        {
+          id: 5032,
+          cod: '85',
+          descricao: 'Conta de 1 a 20.',
+          selected: null,
+        },
+        {
+          id: 5033,
+          cod: '86',
+          descricao:
+            'Identifica objeto que está colocado no meio, em 1o lugar e último.',
+          selected: null,
+        },
+        {
+          id: 5034,
+          cod: '87',
+          descricao:
+            'Conta até 20 objetos e responde a pergunta: “quantos objetos você contou?”',
+          selected: null,
+        },
+        {
+          id: 5035,
+          cod: '88',
+          descricao: 'Nomeia 10 numerais.',
+          selected: null,
+        },
+        {
+          id: 5036,
+          cod: '89',
+          descricao: 'Identifica qual a sua esquerda e direita.',
+          selected: null,
+        },
+        {
+          id: 5037,
+          cod: '90',
+          descricao: 'Diz as vogais em ordem.',
+          selected: null,
+        },
+        {
+          id: 5038,
+          cod: '91',
+          descricao: 'Escreve seu nome com letra de forma.',
+          selected: null,
+        },
+        {
+          id: 5039,
+          cod: '92',
+          descricao: 'Nomeia 5 letras do alfabeto.',
+          selected: null,
+        },
+        {
+          id: 5040,
+          cod: '93',
+          descricao: 'Ordena objetos em sequência de comprimento e largura.',
+          selected: null,
+        },
+        {
+          id: 5041,
+          cod: '94',
+          descricao: 'Nomeia as letras maiúsculas do alfabeto.',
+          selected: null,
+        },
+        {
+          id: 5042,
+          cod: '95',
+          descricao: 'Coloca numerais na sequência de 1 a 10 corretamente',
+          selected: null,
+        },
+        {
+          id: 5043,
+          cod: '96',
+          descricao:
+            'Identifica a posição de objetos como estando em 1º, 2º e 3º lugar.',
+          selected: null,
+        },
+        {
+          id: 5044,
+          cod: '97',
+          descricao: 'Nomeia as letras minúsculas do alfabeto.',
+          selected: null,
+        },
+        {
+          id: 5045,
+          cod: '98',
+          descricao: 'Emaparelha letras maiúsculas com minúsculas.',
+          selected: null,
+        },
+        {
+          id: 5046,
+          cod: '99',
+          descricao: 'Aponta para numerais de 1 a 25.',
+          selected: null,
+        },
+        {
+          id: 5047,
+          cod: '100',
+          descricao: 'Copia um losango.',
+          selected: null,
+        },
+        {
+          id: 5048,
+          cod: '101',
+          descricao: 'Completa um labirinto simples.',
+          selected: null,
+        },
+        {
+          id: 5049,
+          cod: '102',
+          descricao: 'Diz os dias da semana em ordem.',
+          selected: null,
+        },
+        {
+          id: 5050,
+          cod: '103',
+          descricao: 'Soma e substrai combinações de até 3 elementos.',
+          selected: null,
+        },
+        {
+          id: 5051,
+          cod: '104',
+          descricao: 'Diz o dia e mês do seu aniversário.',
+          selected: null,
+        },
+        {
+          id: 5052,
+          cod: '105',
+          descricao: 'Lê 10 palavras impressas.',
+          selected: null,
+        },
+        {
+          id: 5053,
+          cod: '106',
+          descricao: 'Prediz o que vai ocorrer.',
+          selected: null,
+        },
+        {
+          id: 5054,
+          cod: '107',
+          descricao: 'Aponta para objetos inteiros e partidos ao meio.',
+          selected: null,
+        },
+        {
+          id: 5055,
+          cod: '108',
+          descricao: 'Conta de memória de 1 a 100.',
+          selected: null,
+        },
+      ],
+    },
+
+    {
+      titulo: 'Linguagem',
+      tipo: 3,
+      objetivos: [
+        {
+          id: 5056,
+          cod: '71',
+          descricao: 'Obedece a uma sequência envolvendo 3 ordens.',
+          selected: null,
+        },
+        {
+          id: 5057,
+          cod: '72',
+          descricao:
+            'Demonstra compreensão elementar de verbos reflexivos, usando-os ocasionalmente ao falar (ela se machucou).',
+          selected: null,
+        },
+        {
+          id: 5058,
+          cod: '73',
+          descricao:
+            'Consegue identificar objetos/figuras que formem pares sob solicitação.',
+          selected: null,
+        },
+        {
+          id: 5059,
+          cod: '74',
+          descricao: 'Emprega o futuro ao falar.',
+          selected: null,
+        },
+        {
+          id: 5060,
+          cod: '75',
+          descricao:
+            'Usa orações compostas por ordenação (“chutei a bola e ela foi parar na rua”).',
+          selected: null,
+        },
+        {
+          id: 5061,
+          cod: '76',
+          descricao:
+            'Consegue identificar a parte de cima e de baixo de objetos, quando solicitado.',
+          selected: null,
+        },
+        {
+          id: 5062,
+          cod: '77',
+          descricao:
+            'Emprega, ocasionalmente, o condicional, ao falar (poderia, pudesse, iria, seria, faria).',
+          selected: null,
+        },
+        {
+          id: 5063,
+          cod: '78',
+          descricao: 'Consegue identificar absurdos em figuras.',
+          selected: null,
+        },
+        {
+          id: 5064,
+          cod: '79',
+          descricao: 'Emprega as seguintes palavras: irmão, irmã, avô, avó.',
+          selected: null,
+        },
+        {
+          id: 5065,
+          cod: '80',
+          descricao:
+            'Completa frases com antônimos ( o irmão é homem, a irmã é....).',
+          selected: null,
+        },
+        {
+          id: 5066,
+          cod: '81',
+          descricao: 'Relata uma história conhecida, sem ajuda de figuras.',
+          selected: null,
+        },
+        {
+          id: 5067,
+          cod: '82',
+          descricao:
+            'Em uma figura, nomeia o objeto que não pertence a uma determinada categoria ( o que não é bicho etc).',
+          selected: null,
+        },
+        {
+          id: 5068,
+          cod: '83',
+          descricao: 'Diz se duas palavras rimam ou não.',
+          selected: null,
+        },
+        {
+          id: 5069,
+          cod: '84',
+          descricao:
+            'Usa frases complexas, compostas por subordinação (“ela quer que eu entre porque....”).',
+          selected: null,
+        },
+        {
+          id: 5070,
+          cod: '85',
+          descricao: 'Diz se um som é forte ou fraco.',
+          selected: null,
+        },
+      ],
+    },
+
+    {
+      titulo: 'Autocuidado',
+      tipo: 4,
+      objetivos: [
+        {
+          id: 5071,
+          cod: '68',
+          descricao:
+            'Limpa o que derramou, indo buscar um pano por conta própria.',
+          selected: null,
+        },
+        {
+          id: 5072,
+          cod: '69',
+          descricao: 'Evita veneno e todas as substâncias prejudiciais.',
+          selected: null,
+        },
+        {
+          id: 5073,
+          cod: '70',
+          descricao: 'Desabotoa sua roupa.',
+          selected: null,
+        },
+        {
+          id: 5074,
+          cod: '71',
+          descricao: 'Abotoa sua roupa.',
+          selected: null,
+        },
+        {
+          id: 5075,
+          cod: '72',
+          descricao: 'Retira prato e talheres da mesa.',
+          selected: null,
+        },
+        {
+          id: 5076,
+          cod: '73',
+          descricao: 'Encaixa zíper em sua terminação.',
+          selected: null,
+        },
+        {
+          id: 5077,
+          cod: '74',
+          descricao: 'Lava as mãos e o rosto.',
+          selected: null,
+        },
+        {
+          id: 5078,
+          cod: '75',
+          descricao: 'Usa talher apropriado para se alimentar.',
+          selected: null,
+        },
+        {
+          id: 5079,
+          cod: '76',
+          descricao:
+            'Acorda durante a noite para ir ao banheiro ou permanece seco toda a noite.',
+          selected: null,
+        },
+        {
+          id: 5080,
+          cod: '77',
+          descricao: 'Limpa e assoa o nariz em 75% das vezes, sem lembretes.',
+          selected: null,
+        },
+        {
+          id: 5081,
+          cod: '78',
+          descricao:
+            'Toma banho sozinho precisando de ajuda apenas para lavar as costas, pescoço e orelhas.',
+          selected: null,
+        },
+        {
+          id: 5082,
+          cod: '79',
+          descricao: 'Usa faca para espalhar manteiga no pão.',
+          selected: null,
+        },
+        {
+          id: 5083,
+          cod: '80',
+          descricao:
+            'Aperta e afrouxa cintos ou fivelas nos vestidos ou calças e sapatos.',
+          selected: null,
+        },
+        {
+          id: 5084,
+          cod: '81',
+          descricao:
+            'Veste-se sozinho, incluindo botões, fechos, cintos e fivelas dianteiros, mas não dá laços.',
+          selected: null,
+        },
+        {
+          id: 5085,
+          cod: '82',
+          descricao:
+            'Serve-se á mesa, enquanto um dos pais segura a travessa de comida.',
+          selected: null,
+        },
+        {
+          id: 5086,
+          cod: '83',
+          descricao:
+            'Ajuda a pôr a mesa, colocando corretamente pratos, talheres e guardanapos, quando recebe instruções.',
+          selected: null,
+        },
+        {
+          id: 5087,
+          cod: '84',
+          descricao: 'Escova os dentes.',
+          selected: null,
+        },
+        {
+          id: 5088,
+          cod: '85',
+          descricao:
+            'Vai até o banheiro a tempo, retira a roupa, usa papel higiênico, dá descarga e veste-se sem ajuda.',
+          selected: null,
+        },
+        {
+          id: 5089,
+          cod: '86',
+          descricao: 'Penteia ou escova cabelos compridos.',
+          selected: null,
+        },
+        {
+          id: 5090,
+          cod: '87',
+          descricao: 'Pendura roupas em cabides.',
+          selected: null,
+        },
+        {
+          id: 5091,
+          cod: '88',
+          descricao: 'Anda pela vizinhança sem supervisão constante.',
+          selected: null,
+        },
+        {
+          id: 5092,
+          cod: '89',
+          descricao: 'Enfia cordões em sapatos/tênis.',
+          selected: null,
+        },
+        {
+          id: 5093,
+          cod: '90',
+          descricao: 'Amarra ou dá laços nos cordões dos sapatos/ tênis.',
+          selected: null,
+        },
+      ],
+    },
+
+    {
+      titulo: 'Motor',
+      tipo: 5,
+      objetivos: [
+        {
+          id: 4064,
+          cod: '96',
+          descricao: 'Fica em um só pé, sem apoio, por quatro a oito segundos.',
+          selected: null,
+        },
+        {
+          id: 4065,
+          cod: '97',
+          descricao: 'Muda de direção ao correr.',
+          selected: null,
+        },
+        {
+          id: 4066,
+          cod: '98',
+          descricao: 'Anda sobre uma viga ou tábua mantendo o equilíbrio.',
+          selected: null,
+        },
+        {
+          id: 4067,
+          cod: '99',
+          descricao: 'Pula para frente dez vezes sem cair.',
+          selected: null,
+        },
+        {
+          id: 4068,
+          cod: '100',
+          descricao: 'Salta sobre uma corda suspensa a 5cm do solo.',
+          selected: null,
+        },
+        {
+          id: 4069,
+          cod: '101',
+          descricao: 'Pula de costas 6 vezes.',
+          selected: null,
+        },
+        {
+          id: 4070,
+          cod: '102',
+          descricao: 'Rebate e apanha uma bola grande.',
+          selected: null,
+        },
+        {
+          id: 4071,
+          cod: '103',
+          descricao: 'Une 2 a 3 pedaços de massa de modelar.',
+          selected: null,
+        },
+        {
+          id: 4072,
+          cod: '104',
+          descricao: 'Recorta em torno de linhas curvas.',
+          selected: null,
+        },
+        {
+          id: 4073,
+          cod: '105',
+          descricao: 'Encaixa objetos de rosca.',
+          selected: null,
+        },
+        {
+          id: 4074,
+          cod: '106',
+          descricao: 'Desce escadas alternando os pés.',
+          selected: null,
+        },
+        {
+          id: 4075,
+          cod: '107',
+          descricao: 'Pedala um triciclo fazendo curvas.',
+          selected: null,
+        },
+        {
+          id: 4076,
+          cod: '108',
+          descricao: 'Salta em um só pé, 5 vezes consecutivas.',
+          selected: null,
+        },
+        {
+          id: 4077,
+          cod: '109',
+          descricao: 'Recorta um círculo de 5cm.',
+          selected: null,
+        },
+        {
+          id: 4078,
+          cod: '110',
+          descricao:
+            'Desenha figuras simples, facilmente identificáveis como casa, homem,árvore.',
+          selected: null,
+        },
+        {
+          id: 4079,
+          cod: '111',
+          descricao: 'Recorta e cola formas simples.',
+          selected: null,
+        },
+      ],
+    },
+  ],
+};
+
+export { portageQuatroHaCincoAno };
