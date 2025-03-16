112822be33a68fce69008db2da3aaae66e61e1bc
Michael Sousa <msousacode@gmail.com>
2025-03-15 22:59:38 -0300

    dev

--- src/pages/avaliacoes/ablls/data/imitacaoMotora.ts
+++ src/pages/avaliacoes/ablls/data/imitacaoMotora.ts
@@ -4,12 +4,16 @@ const imitacaoMotora = [
     cod: 'D1',
     descricao: 'Imitação motora usando objetos',
     pontos: 4,
+    pontuacao:
+      '4= Pelo menos 10 ações com pelo menos 2 ações diferentes por objeto \n3= 10 ações \n2= 5 ações \n1= 2 ações',
   },
   {
     id: 301,
     cod: 'D2',
     descricao: 'Imitação motora usando objetos em discriminação',
     pontos: 4,
+    pontuacao:
+      '4= 10 ações e sempre imita ações novas \n3= 10 ações \n2= 5 ações \n1= 2 ações',
   },
   {
     id: 302,
@@ -17,18 +21,24 @@ const imitacaoMotora = [
     descricao:
       'Imitação motora de movimentos motores grossos com solicitações verbais',
     pontos: 4,
+    pontuacao:
+      '4= Pelo menos 10 ações e sempre imita ações novas \n3= imita 10 ações \n2= 5 ações imitadas \n1= 2 ações',
   },
   {
     id: 303,
     cod: 'D4',
     descricao: 'Imitar movimentos de pé e perna',
     pontos: 4,
+    pontuacao:
+      '4= Pelo menos 10 ações e sempre imita ações novas \n3= imita 10 ações \n2= 5 ações imitadas \n1= 2 ações',
   },
   {
     id: 304,
     cod: 'D5',
     descricao: 'Imitar movimentos de braços e mãos',
     pontos: 4,
+    pontuacao:
+      '4= Pelo menos 10 ações e sempre imita ações novas \n3= imita 10 ações \n2= 5 ações imitadas \n1= 2 ações',
   },
   {
     id: 305,
@@ -36,54 +46,70 @@ const imitacaoMotora = [
     descricao:
       'Imitar movimentos motores estáticos e cinéticos em discriminação',
     pontos: 4,
+    pontuacao:
+      '4= Pelo menos 10 pares de ações e imita ações novas \n3= imita 10 pares de ações \n2= imita 5 pares de ações \n1= imita 2 pares de ações similares quando uma resposta do par inclui uma ação em movimento e a outra uma ação estacionária.',
   },
   {
     id: 306,
     cod: 'D7',
     descricao: 'Instruções variadas para imitar',
     pontos: 2,
+    pontuacao:
+      '2= imita ações conhecidas dentre 4 ações diferentes \n1= imita ações mediante pelo menos 2 instruções diferentes.',
   },
   {
     id: 307,
     cod: 'D8',
     descricao: 'Imitar ações motoras grossas feitas em um espelho',
     pontos: 4,
+    pontuacao:
+      '4= Pelo menos 10 ações e imita ações novas \n3= imita 10 ações \n2= imita 4 ações \n1= imita 2 ações',
   },
   {
     id: 308,
     cod: 'D9',
     descricao: 'Imitar movimentos com a cabeça',
     pontos: 2,
+    pontuacao: '2= imita 3 ações \n1= imita 1 ação',
   },
   {
     id: 309,
     cod: 'D10',
     descricao: 'Imitar movimentos de boca e língua',
     pontos: 2,
+    pontuacao: '2= imita 4 ações \n1= 2 ações',
   },
   {
     id: 310,
     cod: 'D11',
     descricao: 'Imitar movimentos faciais/orais feitos em um espelho',
     pontos: 4,
+    pontuacao:
+      '4= pelo menos 6 movimentos da boca incluindo movimentos cinéticos e estáticos \n3= imita 6 ações \n2= imita 4 ações \n1= 2 ações',
   },
   {
     id: 311,
     cod: 'D12',
     descricao: 'Imitação motora de movimentos finos',
     pontos: 4,
+    pontuacao:
+      '4= 10 ações e imita a ações novas \n3= 10 ações \n2= 5 ações \n1= 2 ações',
   },
   {
     id: 312,
     cod: 'D13',
     descricao: 'Imitação de tocar objetos em sequencia',
     pontos: 2,
+    pontuacao:
+      '2= Pode imitar uma sequencia tocando 4 de 6 objetos \n1= Pode imitar uma sequencia tocando 2 de 4 objetos',
   },
   {
     id: 313,
     cod: 'D14',
     descricao: 'Imitar assoprar',
     pontos: 2,
+    pontuacao:
+      '2= Imita as duas ações (curta e moderada) \n1= Pode imita somente uma das duas ações.',
   },
   {
     id: 314,
@@ -91,6 +117,8 @@ const imitacaoMotora = [
     descricao:
       'Imitar a velocidade de ações que estão acontecendo na hora com objetos',
     pontos: 2,
+    pontuacao:
+      '2= Imita as ações até coincidir com a velocidade do modelo \n1= Necessita somente de uma ajuda verbal para fazer coincidir com a velocidade do modelo.',
   },
   {
     id: 315,
@@ -98,6 +126,8 @@ const imitacaoMotora = [
     descricao:
       'Imitar a velocidade de uma ação modelada recentemente com objetos',
     pontos: 2,
+    pontuacao:
+      '2= Imita as ações com objetos e iguala a velocidade da ação imediatamente depois do modelo \n1= Precisa de ajuda verbal para igualar a velocidade de uma ação com um objeto depois de realizar o modelo.',
   },
   {
     id: 316,
@@ -105,66 +135,88 @@ const imitacaoMotora = [
     descricao:
       'Imitar a velocidade de uma ação modelada recentemente com objetos',
     pontos: 2,
+    pontuacao:
+      '2= Sempre imita ações na mesma velocidade do modelo \n1= Necessita ajuda verbal para imitar a velocidade do modelo',
   },
   {
     id: 317,
     cod: 'D18',
     descricao: 'Imitar tocar sequência seguindo um modelo',
     pontos: 2,
+    pontuacao:
+      '2= Imita a ordem com 4 de 6 objetos imediatamente depois do modelo \n1= Pode imitar a ordem com 2 de 4 objetos imediatamente depois do modelo.',
   },
   {
     id: 318,
     cod: 'D19',
     descricao: 'Imitar uma sequência de ações mudando com o modelo',
     pontos: 2,
+    pontuacao:
+      '2= Pode imitar uma sequencia de 6 ações diferentes em 10 segundos seguindo o modelo \n1= Pode imitar uma sequencia de 4 ações diferentes em 10 segundos',
   },
   {
     id: 319,
     cod: 'D20',
     descricao: 'Imitar uma sequência de ações',
     pontos: 4,
+    pontuacao:
+      '4= Pelo menos 10 sequencias e sempre imita sequencias novas \n3= 10 sequencias de 10 ações \n2= 5 sequencias de 2 ações \n1= 2 sequencias de 2 ações depois de receber as instruções da sequencia das ações e do modelo.',
   },
   {
     id: 320,
     cod: 'D21',
     descricao: 'Imitar a intensidade de uma ação',
     pontos: 2,
+    pontuacao:
+      '2= imita o movimento e a intensidade \n1= Precisa de ajuda verbal para imitar a intensidade',
   },
   {
     id: 321,
     cod: 'D22',
     descricao: 'Imitar o número de repetições de um movimento motor',
     pontos: 2,
+    pontuacao:
+      '2= Imita de uma a três repetições de um modelo sem ajuda. \n1= imita o numero de 1 a 3 repetições com ajuda verbal ou gestual',
   },
   {
     id: 322,
     cod: 'D23',
     descricao: 'Imitar simultaneamente o movimento motor e uma vocalização',
     pontos: 2,
+    pontuacao:
+      '2= imita até três repetições de pelo menos 4 respostas combinadas de ações e verbalizações sem ajuda \n1= imita pelo menos uma resposta combinada de ação e vocalização sem ajuda.',
   },
   {
     id: 323,
     cod: 'D24',
     descricao: 'Imitar uma sequência motora usando múltiplos objetos',
     pontos: 2,
+    pontuacao:
+      '2= pode repetir pelo menos 6 sequencias diferentes de pelo menos 3 ações que incluem 4 objetos com o modelo original visível \n1= pode repetir pelo menos 6 sequencias diferentes de pelo menos 2 ações e 4 objetos com o modelo original visível.',
   },
   {
     id: 324,
     cod: 'D25',
     descricao: 'Imitar movimentos motores sem direta instrução verbal',
     pontos: 4,
+    pontuacao:
+      '4= Pelo menos 10 ações se sempre imita ações novas \n3= 10 ações \n2= 5 ações \n1= 2 ações',
   },
   {
     id: 325,
     cod: 'D26',
     descricao: 'Imitar espontaneamente ações dos outros',
     pontos: 4,
+    pontuacao:
+      '4= Ao menos 10 ações e sempre imita novas ações (não é necessário ter uma sequencia exata para estas 10 ações). \n3= 10 ações \n2= 5 ações \n1= 2 ações',
   },
   {
     id: 326,
     cod: 'D27',
     descricao: 'Imitar com atraso',
     pontos: 4,
+    pontuacao:
+      '4= Nomeia e imita varias ações observadas nas últimas 4 horas \n3= Nomeia e imita depois de uma hora \n2= Nomeia e imita depois de 10 minutos \n1= Quando pedido, imita uma ação a cada 5 minutos',
   },
 ];
 
--- src/pages/avaliacoes/ablls/data/imitacaoVocal.ts
+++ src/pages/avaliacoes/ablls/data/imitacaoVocal.ts
@@ -1,124 +1,164 @@
 const imitacaoVocal = [
   {
     id: 400,
-    cod: 'D1',
+    cod: 'E1',
     descricao: 'Imitar sons quando solicitado',
     pontos: 4,
+    pontuacao:
+      '4= Sempre imita qualquer som corretamente \n3= 15 sons \n2= 5 sons \n1= 2 sons',
   },
   {
     id: 401,
-    cod: 'D2',
+    cod: 'E2',
     descricao: 'Imitar uma sequência de sons alternando com um modelo',
     pontos: 2,
+    pontuacao:
+      '2= Pode repetir pelo menos 10 sons separados em 15 segundos \n1= Pode repetir 5 sons separados em 10 segundos',
   },
   {
     id: 402,
-    cod: 'D3',
+    cod: 'E3',
     descricao: 'Imitar som inicial das palavras',
     pontos: 4,
+    pontuacao:
+      '4= Sempre imita pelo menos o primeiro som de qualquer palavra corretamente \n3= 15 sons \n2= 5 sons \n1= 2 sons',
   },
   {
     id: 403,
-    cod: 'D4',
+    cod: 'E4',
     descricao: 'Imitar múltiplas combinações de sons separados',
     pontos: 2,
+    pontuacao:
+      '2= Pode repetir ao menos 4 pares de combinações de sons de pelo menos 4 sons. \n1= Precisa de ajuda gestual para fazer coincidir a velocidade do modelo.',
   },
   {
     id: 404,
-    cod: 'D5',
+    cod: 'E5',
     descricao: 'Imitar sons curtos e rápidos vs. Sons compridos e lentos',
     pontos: 2,
+    pontuacao:
+      '2= Imita os sons fazendo coincidir a velocidade do modelo \n1= Precisa de ajuda gestual para fazer coincidir a velocidade do modelo.',
   },
   {
     id: 405,
-    cod: 'D6',
+    cod: 'E6',
     descricao: 'Imitar o número de repetições de um som',
     pontos: 2,
+    pontuacao:
+      '2= Imita até três repetições de um som sem ajuda \n1= Imita de uma em uma três repetições de um som quando lhe dá ajuda gestual.',
   },
   {
     id: 406,
-    cod: 'D7',
+    cod: 'E7',
     descricao: 'Imitar um som segurando para um segundo som',
     pontos: 2,
+    pontuacao:
+      '2= Pode imitar a troca de pelo menos 4 sons. \n1= Pelo menos 2 trocas.',
   },
   {
     id: 407,
-    cod: 'D8',
+    cod: 'E8',
     descricao: 'Combinações consoante / vogal, vogal / consoante',
     pontos: 2,
+    pontuacao:
+      '2= Pode repetir pelo menos 3 combinações de vogalconsoante ou de consoante-vogal, sem descanso entre os dois sons. \n1= Pode repetir pelo menos 3 combinações de consoante-vogal ou vogal-consoante sem descanso entre os dois sons.',
   },
   {
     id: 408,
-    cod: 'D9',
+    cod: 'E9',
     descricao: 'Consoante-vogal consoante-vogal',
     pontos: 2,
+    pontuacao:
+      '2= Pode repetir pelo menos 6 combinações de consoante-vogal-consoante-vogal, sem descanso entre os dois sons \n1= Pode repetir pelo menos 3 combinações de consoante-vogal ou vogal-consoante, sem descanso entre os dois sons.',
   },
   {
     id: 409,
-    cod: 'D10',
+    cod: 'E10',
     descricao: 'Combinação Consoante-vogal consoante',
     pontos: 2,
+    pontuacao:
+      '2= Pode repetir pelo menos 12 combinações de consoante-vogal-consoante, sem descanso entre os dois sons \n1= Pode repetir pelo menos 3 combinações de consoante-vogal-consoante sem descanso entre os dois sons.',
   },
   {
     id: 410,
-    cod: 'D11',
+    cod: 'E11',
     descricao: 'Imitar combinações de consoante',
     pontos: 4,
+    pontuacao:
+      '4= Sempre imita qualquer palavra com combinações de consoantes, em qualquer posição, corretamente \n3= 15 palavras nas mesmas condições \n2= 5 palavras \n1= 2 palavras',
   },
   {
     id: 411,
-    cod: 'D12',
+    cod: 'E12',
     descricao: 'Imitar palavras quando solicitado',
     pontos: 4,
+    pontuacao:
+      '4= Sempre imita qualquer palavra corretamente \n3= Aproximadamente 15 palavras \n2= Aproximadamente 5 palavras \n1= Aproximadamente 2 palavras',
   },
   {
     id: 412,
-    cod: 'D13',
+    cod: 'E13',
     descricao: 'Imitar frases quando solicitado',
     pontos: 4,
+    pontuacao:
+      '4= Sempre imita frases de 6 ou mais palavras corretamente \n3= Frases de 4 palavras \n2= Frases de 3 palavras \n1= Frases de 2 palavras',
   },
   {
     id: 413,
-    cod: 'D14',
+    cod: 'E14',
     descricao: 'Imitar sequências de número quando solicitado',
     pontos: 4,
+    pontuacao:
+      '4= Sempre imita uma sequencia de 7 ou mais números \n3= 4 números \n2= 3 números \n1= 2 números',
   },
   {
     id: 414,
-    cod: 'D15',
+    cod: 'E15',
     descricao: 'Imitar palavras curtas e rápido versus longas e devagar',
     pontos: 2,
+    pontuacao:
+      '2= Imita as palavras igualando a velocidade do modelo \n1= Precisa de ajuda para fazer isso',
   },
   {
     id: 415,
-    cod: 'D16',
+    cod: 'E16',
     descricao:
       'Imitar sons e palavras em volume alto versus sons e palavras em volume baixo',
     pontos: 2,
+    pontuacao:
+      '2= Imita as palavras igualando o volume do modelo \n1= Precisa de ajuda para fazer isso',
   },
   {
     id: 416,
-    cod: 'D17',
+    cod: 'E17',
     descricao: 'Imitar sons e palavras agudas versus graves',
     pontos: 2,
+    pontuacao:
+      '2= Imita as palavras igualando a entonação do modelo \n1= Precisa de ajuda para fazer isso',
   },
   {
     id: 417,
-    cod: 'D18',
+    cod: 'E18',
     descricao: 'Repetir mensagens curtas para uma outra pessoa',
     pontos: 2,
+    pontuacao:
+      '2= Pode repetir uma frase de três palavras depois de se afastar pelo menos 10 metros para repassar o conteúdo da mensagem. \n2= Pode repetir uma frase de duas palavras depois de se afastar pelo menos 5 metros para repassar o conteúdo da mensagem.',
   },
   {
     id: 418,
-    cod: 'D19',
+    cod: 'E19',
     descricao: 'Imitar espontaneamente palavras',
     pontos: 4,
+    pontuacao:
+      '4= Imita pelo menos 20 palavras por dia corretamente \n3= 15 palavras \n2= 5 palavras \n1= 2 palavras por dia',
   },
   {
     id: 419,
-    cod: 'D20',
+    cod: 'E20',
     descricao: 'Imitar frases espontaneamente',
     pontos: 4,
+    pontuacao:
+      '4= Imita frases de 6 ou mais palavras pelo menos 3 vezes por dia corretamente \n3= Frases de 4 palavras 3 vezes ao dia \n2= Frases de 3 palavras 3 vezes por dia \n1=Frases de duas palavras pelo menos 3 vezes ao dia.',
   },
 ];
 
