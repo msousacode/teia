024f559ddd6d5bb5e6b98ebd1db116e182d1727f
MSousa <msousacode@gmail.com>
2025-09-05 07:48:29 -0300

    commit.

--- src/stores/objetivos.ts
+++ src/stores/objetivos.ts
@@ -3,17 +3,24 @@ import { defineStore } from 'pinia';
 export const useObjetivosStore = defineStore('objetivos', {
   state: () => ({
     data: [],
+    importadosList: [],
   }),
 
   getters: {
     getData(state) {
       return state.data;
     },
+    getImportadosList(state) {
+      return state.importadosList;
+    },
   },
 
   actions: {
     setData(_data: []) {
       this.data = _data;
+    },
+    setImportList(_importadosList: []) {
+      this.importadosList = _importadosList;
     },
   },
 });
